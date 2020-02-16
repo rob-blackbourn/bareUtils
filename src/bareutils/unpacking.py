fe87e21cadfc49c5f0e023f4e7651012d52e7a42
Rob Blackbourn <rob.blackbourn@googlemail.com>
2020-02-16 10:54:02 +0000

    Added google docstrings

--- src/bareutils/unpacking.py
+++ src/bareutils/unpacking.py
@@ -9,26 +9,29 @@ from baretypes import Content
 MessageParams = List[Tuple[str, str]]
 MessagePayload = Union[List[Message], str, bytes, None]
 
+
 async def unpack_multipart_form_data(
         content_type: bytes,
         content: Content
 ) -> List[Tuple[MessageParams, MessagePayload]]:
     """Unpack multipart form data
 
-    :param content_type: The 'content-type' header
-    :type content_type: bytes
-    :param content: The content to parse.
-    :type content: Content
-    :raises AssertionError: When the problems were found with the data
-    :return: The form and files
-    :rtype: List[Tuple[MessageParams, MessagePayload]]
+    Args:
+        content_type (bytes): The 'content-type' header
+        content (Content): The content to parse.
+
+    Raises:
+        AssertionError: When the problems were found with the data
+
+    Returns:
+        List[Tuple[MessageParams, MessagePayload]]: The form and files
     """
     # Create the parser and prime it with the content
     # type.
     parser = BytesFeedParser()
-    parser.feed(b'Content-Type: ') # type: ignore[arg-type]
-    parser.feed(content_type) # type: ignore[arg-type]
-    parser.feed(b'\r\n\r\n') # type: ignore[arg-type]
+    parser.feed(b'Content-Type: ')  # type: ignore[arg-type]
+    parser.feed(content_type)  # type: ignore[arg-type]
+    parser.feed(b'\r\n\r\n')  # type: ignore[arg-type]
 
     # Feed the content to the parser.
     async for buf in content:
@@ -44,7 +47,8 @@ async def unpack_multipart_form_data(
 
     data: List[Tuple[MessageParams, MessagePayload]] = []
     for msg_part in msg_payload:
-        assert isinstance(msg_part, Message), "A message part should also be a message"
+        assert isinstance(
+            msg_part, Message), "A message part should also be a message"
         assert not msg_part.is_multipart(), "A message part should not be multipart"
         assert not msg_part.defects, "A message part should not have defects"
 
