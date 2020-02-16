4c8e3d53d1556b1815ba1229d181e5956ec92049
Rob Blackbourn <rob.blackbourn@googlemail.com>
2020-02-16 10:52:15 +0000

    Added google docstrings

--- src/bareutils/streaming.py
+++ src/bareutils/streaming.py
@@ -1,13 +1,19 @@
+"""Streaming"""
+
 from __future__ import annotations
 import codecs
+
 from baretypes import Content
 
 
 async def bytes_reader(content: Content) -> bytes:
     """Extracts the body content as bytes.
 
-    :param content: The content argument of the request handler.
-    :return: The body as bytes.
+    Args:
+        content (Content): The content argument of the request handler.
+
+    Returns:
+        bytes: The body as bytes.
     """
     buf = b''
     async for b in content:
@@ -18,9 +24,12 @@ async def bytes_reader(content: Content) -> bytes:
 async def text_reader(content: Content, encoding: str = 'utf-8') -> str:
     """Extracts the body contents as text.
 
-    :param content: The content argument of the request handler.
-    :param encoding: The encoding of the text (defaults to 'utf-8').
-    :return: The body contents as a string.
+    Args:
+        content (Content): The content argument of the request handler.
+        encoding (str, optional): The encoding of the text. Defaults to 'utf-8'.
+
+    Returns:
+        str: The body contents as a string.
     """
     codec_info: codecs.CodecInfo = codecs.lookup(encoding)
     decoder = codec_info.incrementaldecoder()
@@ -31,13 +40,16 @@ async def text_reader(content: Content, encoding: str = 'utf-8') -> str:
 
 
 async def bytes_writer(buf: bytes, chunk_size: int = -1) -> Content:
-    """Creates an asynchronous generator from the supplied response body.
+    """Creates an asynchronous iterator from the supplied response body.
 
-    :param buf: The response body to return.
-    :param chunk_size: The size of each chunk to send or -1 to send as a single chunk.
-    :return: An asynchronous generator of bytes.
+    Args:
+        buf (bytes): The response body to return.
+        chunk_size (int, optional): The size of each chunk to send or -1 to send
+            as a single chunk.. Defaults to -1.
+
+    Yields:
+        Content: The body bytes
     """
-
     if chunk_size == -1:
         yield buf
     else:
@@ -47,15 +59,23 @@ async def bytes_writer(buf: bytes, chunk_size: int = -1) -> Content:
             start, end = end, end + chunk_size
 
 
-async def text_writer(text: str, encoding: str = 'utf-8', chunk_size: int = -1) -> Content:
-    """Creates an asynchronous generator from the supplied response body.
+async def text_writer(
+        text: str,
+        encoding: str = 'utf-8',
+        chunk_size: int = -1
+) -> Content:
+    """Creates an asynchronous iterator from the supplied response body.
 
-    :param text: The response body.
-    :param encoding:  The encoding to apply when transforming the text into bytes (defaults to 'utf-8').
-    :param chunk_size: The size of each chunk to send or -1 to send as a single chunk.
-    :return: An asynchronous generator of bytes.
+    Args:
+        text (str): The response body.
+        encoding (str, optional): The encoding to apply when transforming the
+            text into bytes. Defaults to 'utf-8'.
+        chunk_size (int, optional): The size of each chunk to send or -1 to send
+            as a single chunk.. Defaults to -1.
+
+    Yields:
+        Content: The body bytes
     """
-
     if chunk_size == -1:
         yield text.encode(encoding=encoding)
     else:
